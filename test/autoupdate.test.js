eb74ebbdb9ead0de59dff65637b3827f7ecf2abd
ssh24 <sshasan10@hotmail.com>
2017-07-26 10:04:21 -0400

    Implement indexes feature

---
+++ test/autoupdate.test.js
@@ -0,0 +1,172 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback-connector-couchdb2
+// This file is licensed under the Apache License 2.0.
+// License text available at https://opensource.org/licenses/Apache-2.0
+
+'use strict';
+
+var db, AutoupdateTestFoo, connector;
+var async = require('async');
+var _ = require('lodash');
+var util = require('util');
+var EXPECTED_INDEXES = {};
+
+describe('CouchDB autoupdate', function() {
+  before(function(done) {
+    db = global.getDataSource();
+    var testModelDef = getTestModelDef();
+    AutoupdateTestFoo = db.define('AutoupdateTestFoo', testModelDef.properties,
+      testModelDef.config);
+    connector = db.connector;
+    db.autoupdate(done);
+  });
+
+  it('autoupdate creates indexes when model first created', function(done) {
+    connector.getModelIndexes('AutoupdateTestFoo', function(err, result) {
+      if (err) return done(err);
+      Object.keys(result).length.should.equal(4);
+
+      // result should contain 'name' 'age' 'email' 'loopback__model__name' index
+      EXPECTED_INDEXES = getExpectedIndexesForFirstCreatedModel();
+      async.eachOf(result, assertIndex, done);
+    });
+  });
+
+  it('autoupdate drops and adds indexes', function(done) {
+    // Drop age, name indexes.
+    // Add postcode, fullName indexes.
+    // Keep email
+    var newTestModelDef = getNewTestModelDef();
+
+    AutoupdateTestFoo = db.define('AutoupdateTestFoo', newTestModelDef.properties,
+      newTestModelDef.config);
+    connector = db.connector;
+
+    db.autoupdate(function(err) {
+      if (err) return done(err);
+      connector.getModelIndexes('AutoupdateTestFoo', function(err, result) {
+        if (err) return done(err);
+        // result should contain 'email', 'fullName_index', 'postcode', 'loopback__model__name'
+        // should not contain 'age', 'name_index'
+        Object.keys(result).length.should.equal(4);
+        EXPECTED_INDEXES = getExpectedIndexesForUpdatedModel();
+        async.eachOf(result, assertIndex, done);
+      });
+    });
+  });
+});
+
+function getTestModelDef() {
+  return {
+    properties: {
+      email: {type: 'string', index: true},
+      age: {type: 'number', index: true},
+      firstName: {type: 'string'},
+      lastName: {type: 'string'},
+    },
+    config: {
+      indexes: {
+        'name_index': {
+          keys: {
+            firstName: 1,
+            lastName: 1,
+          },
+        },
+      },
+    },
+  };
+}
+
+function getNewTestModelDef() {
+  return {
+    properties: {
+      email: {type: 'string', index: true},
+      age: {type: 'number'},
+      postcode: {type: 'string', index: true},
+      firstName: {type: 'string'},
+      middleName: {type: 'string'},
+      lastName: {type: 'string'},
+    },
+    config: {
+      indexes: {
+        'fullName_index': {
+          keys: {
+            firstName: 1,
+            middleName: 1,
+            lastName: 1,
+          },
+        },
+      },
+    },
+  };
+}
+
+function getExpectedIndexesForFirstCreatedModel() {
+  /* eslint camelcase: ["error", {properties: "never"}] */
+  var result = {
+    age_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__age_index',
+      fields: [{age: 'asc'}],
+    },
+    email_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__email_index',
+      fields: [{email: 'asc'}],
+    },
+    name_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__name_index',
+      fields: [{firstName: 'asc'}, {lastName: 'asc'}],
+    },
+    loopback__model__name_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__loopback__model__name_index',
+      fields: [{loopback__model__name: 'asc'}],
+    },
+  };
+  return result;
+}
+
+function getExpectedIndexesForUpdatedModel() {
+  var result = {
+    postcode_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__postcode_index',
+      fields: [{postcode: 'asc'}],
+    },
+    email_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__email_index',
+      fields: [{email: 'asc'}],
+    },
+    fullName_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__fullName_index',
+      fields: [{firstName: 'asc'}, {lastName: 'asc'}, {middleName: 'asc'}],
+    },
+    loopback__model__name_index: {
+      ddoc: '_design/LBModel__AutoupdateTestFoo__LBIndex__loopback__model__name_index',
+      fields: [{loopback__model__name: 'asc'}],
+    },
+  };
+  return result;
+}
+
+function assertIndex(value, key, cb) {
+  EXPECTED_INDEXES[key].should.exist;
+  checkDdocname(key, value.ddoc);
+  checkFields(key, value.fields);
+  cb();
+}
+
+function checkDdocname(key, ddocName) {
+  EXPECTED_INDEXES[key].ddoc.should.equal(ddocName);
+}
+
+function checkFields(key, fields) {
+  arrayEqual(EXPECTED_INDEXES[key].fields, fields);
+};
+
+function arrayEqual(expect, actual) {
+  var notEqualMsg = util.inspect(expect, 4) + ' is not equal to ' +
+    util.inspect(actual, 4);
+  for (var item in expect) {
+    var cond = expect[item];
+    var i = _.findIndex(actual, cond);
+    i.should.above(-1, notEqualMsg);
+  }
+}
---
+++ test/index.test.js
@@ -0,0 +1,324 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: loopback-connector-couchdb2
+// This file is licensed under the Apache License 2.0.
+// License text available at https://opensource.org/licenses/Apache-2.0
+
+'use strict';
+
+var _ = require('lodash');
+var should = require('should');
+
+if (!process.env.COUCHDB2_TEST_SKIP_INIT) {
+  require('./init.js');
+}
+
+var connector, db, modelName, Product;
+
+describe('couchdb2 indexes', function() {
+  before(function(done) {
+    db = global.getDataSource();
+    connector = db.connector;
+    modelName = 'Product';
+
+    Product = db.define(modelName, {
+      prodName: {type: String, index: true},
+      prodPrice: {type: Number},
+      prodCode: {type: String},
+    });
+    db.automigrate(done);
+  });
+
+  after(function(done) {
+    Product.destroyAll(done);
+  });
+
+  it('support property level indexes', function(done) {
+    connector.getIndexes(connector.getDbName(connector), function(err, indexes) {
+      should.not.exist(err);
+      indexes = indexes.indexes;
+      var indexName = 'prodName_index';
+
+      should.not.exist(err);
+      should.exist(indexes);
+
+      var index = _.find(indexes, function(index) {
+        return index.name === indexName;
+      });
+
+      should.exist(index);
+      should.exist(index.name);
+      index.name.should.equal(indexName);
+      index.def.fields[0]['prodName'].should.equal('asc');
+      done();
+    });
+  });
+
+  it('support model level indexes', function(done) {
+    Product = db.define(modelName, {
+      prodName: {type: String},
+      prodPrice: {type: Number},
+      prodCode: {type: String},
+    }, {
+      indexes: {
+        'prodPrice_index': {
+          keys: {
+            prodPrice: -1,
+          },
+        },
+      },
+    });
+
+    db.automigrate('Product', function(err) {
+      should.not.exist(err);
+      connector.getIndexes(connector.getDbName(connector), function(err, indexes) {
+        should.not.exist(err);
+        indexes = indexes.indexes;
+        var indexName = 'prodPrice_index';
+
+        should.not.exist(err);
+        should.exist(indexes);
+
+        var index = _.find(indexes, function(index) {
+          return index.name === indexName;
+        });
+
+        should.exist(index);
+        should.exist(index.name);
+        index.name.should.equal(indexName);
+        index.def.fields[0]['prodPrice'].should.equal('desc');
+        done();
+      });
+    });
+  });
+
+  it('support both property and model level indexes', function(done) {
+    Product = db.define(modelName, {
+      prodName: {type: String, index: true},
+      prodPrice: {type: Number},
+      prodCode: {type: String},
+    }, {
+      indexes: {
+        'prodPrice_index': {
+          keys: {
+            prodPrice: -1,
+          },
+        },
+      },
+    });
+
+    db.automigrate('Product', function(err) {
+      should.not.exist(err);
+      connector.getIndexes(connector.getDbName(connector), function(err, indexes) {
+        indexes = indexes.indexes;
+        var priceIndex = 'prodPrice_index';
+        var nameIndex = 'prodName_index';
+
+        should.not.exist(err);
+        should.exist(indexes);
+
+        var priceIndexDoc = _.find(indexes, function(index) {
+          return index.name === priceIndex;
+        });
+
+        var nameIndexDoc = _.find(indexes, function(index) {
+          return index.name === nameIndex;
+        });
+
+        should.exist(priceIndexDoc);
+        should.exist(nameIndexDoc);
+        should.exist(priceIndexDoc.name);
+        should.exist(nameIndexDoc.name);
+        priceIndexDoc.name.should.equal(priceIndex);
+        nameIndexDoc.name.should.equal(nameIndex);
+        priceIndexDoc.def.fields[0]['prodPrice'].should.equal('desc');
+        nameIndexDoc.def.fields[0]['prodName'].should.equal('asc');
+        done();
+      });
+    });
+  });
+
+  it('support multiple property level indexes', function(done) {
+    Product = db.define(modelName, {
+      prodName: {type: String, index: true},
+      prodPrice: {type: Number},
+      prodCode: {type: String, index: true},
+    });
+
+    db.automigrate(function(err) {
+      should.not.exist(err);
+
+      connector.getIndexes(connector.getDbName(connector), function(err, indexes) {
+        should.not.exist(err);
+        should.exist(indexes);
+
+        indexes = indexes.indexes;
+        var nameIndex = 'prodName_index';
+        var codeIndex = 'prodCode_index';
+
+        var nameIndexDoc = _.find(indexes, function(index) {
+          return index.name === nameIndex;
+        });
+
+        var codeIndexDoc = _.find(indexes, function(index) {
+          return index.name === codeIndex;
+        });
+
+        should.exist(codeIndexDoc);
+        should.exist(nameIndexDoc);
+        should.exist(codeIndexDoc.name);
+        should.exist(nameIndexDoc.name);
+        codeIndexDoc.name.should.equal(codeIndex);
+        nameIndexDoc.name.should.equal(nameIndex);
+        codeIndexDoc.def.fields[0]['prodCode'].should.equal('asc');
+        nameIndexDoc.def.fields[0]['prodName'].should.equal('asc');
+        done();
+      });
+    });
+  });
+
+  it('support composite indexes going same direction', function(done) {
+    Product = db.define(modelName, {
+      prodName: {type: String},
+      prodPrice: {type: Number},
+      prodCode: {type: String},
+    }, {
+      indexes: {
+        'price_code_index': {
+          keys: {
+            prodPrice: 1,
+            prodCode: 1,
+          },
+        },
+      },
+    });
+
+    db.automigrate('Product', function(err) {
+      connector.getIndexes(connector.getDbName(connector), function(err, indexes) {
+        indexes = indexes.indexes;
+        var indexName = 'price_code_index';
+
+        should.not.exist(err);
+        should.exist(indexes);
+
+        var index = _.find(indexes, function(index) {
+          return index.name === indexName;
+        });
+
+        should.exist(index);
+        should.exist(index.name);
+        index.name.should.equal(indexName);
+        index.def.fields[0]['prodPrice'].should.equal('asc');
+        index.def.fields[1]['prodCode'].should.equal('asc');
+        done();
+      });
+    });
+  });
+
+  it('coerce order when composite indexes go opposite direction', function(done) {
+    Product = db.define(modelName, {
+      prodName: {type: String, index: true},
+      prodPrice: {type: Number, index: true},
+      prodCode: {type: String},
+    }, {
+      indexes: {
+        'code_price_index': {
+          keys: {
+            prodCode: 1,
+            prodPrice: -1,
+          },
+        },
+      },
+    });
+
+    db.automigrate('Product', function(err) {
+      should.not.exist(err);
+      connector.getIndexes(connector.getDbName(connector), function(err, indexes) {
+        should.exist(indexes);
+        indexes = indexes.indexes;
+        var compositeIndex = 'code_price_index';
+
+        var compositeIndexDoc = _.find(indexes, function(index) {
+          return index.name === compositeIndex;
+        });
+
+        should.exist(compositeIndexDoc);
+        compositeIndexDoc.name.should.equal(compositeIndex);
+        compositeIndexDoc.def.fields[0]['prodCode'].should.equal('asc');
+        compositeIndexDoc.def.fields[1]['prodPrice'].should.equal('asc');
+        done();
+      });
+    });
+  });
+
+  context('query using indexes', function() {
+    before(function(done) {
+      Product.create(data, done);
+    });
+
+    it('couch picked default index', function(done) {
+      Product.find({
+        where: {
+          prodPrice: {
+            gt: 0,
+          },
+        },
+        order: 'prodPrice desc',
+      },
+      function(err, products) {
+        should.not.exist(err);
+        should.exist(products);
+        // check if the prices are in descending order
+        for (var i = 1; i < products.length; i++) {
+          var previous = products[i - 1].prodPrice;
+          var current = products[i].prodPrice;
+          should.ok(previous >= current);
+        }
+        done();
+      });
+    });
+
+    it('user specified index', function(done) {
+      /* eslint camelcase: ["error", {properties: "never"}] */
+      Product.find({where: {prodPrice: {gt: 0}}}, {
+        use_index: 'LBModel__Product__LBIndex__prodPrice_index',
+      }, function(err, products) {
+        should.not.exist(err);
+        should.exist(products);
+        // check if the prices are in ascending order
+        for (var i = 1; i < products.length; i++) {
+          var previous = products[i - 1].prodPrice;
+          var current = products[i].prodPrice;
+          should.ok(previous <= current);
+        }
+        // check if the codes are in ascending order
+        var codes = _.uniq(_.map(products, function(product) {
+          return product.prodCode;
+        }));
+        should.deepEqual(codes, ['abc', 'def', 'ghi']);
+        done();
+      });
+    });
+  });
+});
+
+var data = [{
+  prodName: 'prod1',
+  prodPrice: 5,
+  prodCode: 'abc',
+}, {
+  prodName: 'prod2',
+  prodPrice: 12,
+  prodCode: 'def',
+}, {
+  prodName: 'prod3',
+  prodPrice: 4,
+  prodCode: 'abc',
+}, {
+  prodName: 'prod4',
+  prodPrice: 10,
+  prodCode: 'def',
+}, {
+  prodName: 'prod5',
+  prodPrice: 20,
+  prodCode: 'ghi',
+}];
